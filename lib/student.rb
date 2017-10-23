5a49e50a006f76ead5870d67c60fb38ae5e8db80
Emily Morgan <emorgan05@gmail.com>
2017-10-23 23:26:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,7 @@ class Student
     sql = <<-SQL
       DROP TABLE students
     SQL
-    
+
     DB[:conn].execute(sql)
   end
 
@@ -37,7 +37,7 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
 
-    
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
   def self.create(name:, grade:)
